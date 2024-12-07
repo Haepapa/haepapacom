a0d68e2e6745920e8ae5c1d15fc80033792f0704
timotewb <timote.wb@gmail.com>
2024-12-07 22:06:03 +1300

    contact dev

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,6 +4,7 @@ import "./Common.css";
 import "../index.css";
 
 function Contact(): JSX.Element {
+  const [count, setCount] = React.useState(0);
   return (
     <div className="page-container">
       <div className="page-title">Contact</div>
@@ -20,7 +21,15 @@ function Contact(): JSX.Element {
 
       <div className="form-container">
         <div className="form-heading">Message</div>
-        <input className="form-input-message"></input>
+
+        <div className="stack-container">
+          <textarea
+            className="form-input-message"
+            maxLength={500}
+            onChange={(e) => setCount(e.target.value.length)}
+          ></textarea>
+          Characters {count}/500
+        </div>
       </div>
 
       <div className="form-container">
