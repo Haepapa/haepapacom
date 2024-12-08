6be289ba78ff4ad4160a4ab6b950b5b653124e85
timotewb <timote.wb@gmail.com>
2024-12-09 08:37:59 +1300

    pre-responisve dev

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,8 @@ import styled from "@emotion/styled";
 import ThemeToggler from "./ThemeToggler";
 import GithubIcon from "./images/GithubIcon";
 
-function Navbar(): JSX.Element {
+function NavbarBackup(): JSX.Element {
+  const [isNavExpanded, setIsNavExpanded] = React.useState(true);
   const LinkLine = styled.span({
     width: "3.25rem",
     height: "0.0625rem",
@@ -15,36 +16,66 @@ function Navbar(): JSX.Element {
 
   return (
     <div className="navbar">
-      <div className="link-container">
-        <Link to="about" spy={true} smooth={true} offset={-70} duration={500}>
-          About
-        </Link>
-        <LinkLine />
-      </div>
-      <div className="link-container">
-        <Link
-          to="projects"
-          spy={true}
-          smooth={true}
-          offset={-70}
-          duration={500}
+      <button
+        className="hamburger"
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded);
+        }}
+      >
+        {/* icon from Heroicons.com */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="white"
         >
-          Projects
-        </Link>
-      </div>
-      <div className="link-container">
-        <Link to="contact" spy={true} smooth={true} offset={-70} duration={500}>
-          Contact
-        </Link>
-      </div>
-      <div className="link-container">
-        <GithubIcon />
-      </div>
-      <div className="link-container">
-        <ThemeToggler />
-      </div>
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <span
+        className={isNavExpanded ? "navbar-links expanded" : "navbar-links"}
+      >
+        <div className="link-container">
+          <Link to="about" spy={true} smooth={true} offset={-70} duration={500}>
+            About
+          </Link>
+          <LinkLine />
+        </div>
+        <div className="link-container">
+          <Link
+            to="projects"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+          >
+            Projects
+          </Link>
+        </div>
+        <div className="link-container">
+          <Link
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+          >
+            Contact
+          </Link>
+        </div>
+        <div className="link-container">
+          <GithubIcon />
+        </div>
+        <div className="link-container">
+          <ThemeToggler />
+        </div>
+      </span>
     </div>
   );
 }
 
-export default Navbar;
+export default NavbarBackup;
