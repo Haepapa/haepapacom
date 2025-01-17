d0c9fb73a1bf697b48ce95eb204bb4c4a91b011b
timotewb <timote.wb@gmail.com>
2025-01-17 21:28:19 +1300

    project filter dev

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -3,7 +3,20 @@ import SectionContainer from "./SectionContainer";
 import getProjects from "@/actions/getProjects";
 import { useEffect, useState } from "react";
 import { CardTemplate } from "./CardTemplate";
-import { SimpleGrid } from "@chakra-ui/react";
+import {
+  SimpleGrid,
+  Spacer,
+  Text,
+  createListCollection,
+} from "@chakra-ui/react";
+import {
+  SelectContent,
+  SelectItem,
+  SelectLabel,
+  SelectRoot,
+  SelectTrigger,
+  SelectValueText,
+} from "@/components/ui/select";
 
 export default function ProjectsSection() {
   const [projects, setProjects] = useState<Project[]>([]);
@@ -16,8 +29,40 @@ export default function ProjectsSection() {
     fetchProjects();
   }, []);
 
+  const frameworks = createListCollection({
+    items: [
+      { label: "React.js", value: "react" },
+      { label: "Vue.js", value: "vue" },
+      { label: "Angular", value: "angular" },
+      { label: "Svelte", value: "svelte" },
+    ],
+  });
+
   return (
     <SectionContainer>
+      <SimpleGrid minChildWidth="190px" gap={2} paddingBottom={4}>
+        <Text fontSize="md" fontWeight="bold">
+          What we are working on
+        </Text>
+        <Spacer />
+        <SelectRoot
+          multiple
+          collection={frameworks}
+          size="sm"
+          background={"white"}
+        >
+          <SelectTrigger>
+            <SelectValueText placeholder="Filter Projects" />
+          </SelectTrigger>
+          <SelectContent>
+            {frameworks.items.map((movie) => (
+              <SelectItem item={movie} key={movie.value}>
+                {movie.label}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </SelectRoot>
+      </SimpleGrid>
       <SimpleGrid minChildWidth="190px" gap={2}>
         {projects.map((project) => (
           <CardTemplate
